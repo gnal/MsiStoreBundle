a3deeb33e278c7ca7c5b51a9682cc282b3c30cae
Aleksis Joubert <qwertypianox@gmail.com>
2013-04-19 08:57:26 -0400

    fix

--- Controller/CheckoutController.php
+++ Controller/CheckoutController.php
@@ -6,14 +6,73 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class CheckoutController extends Controller
 {
-    public function reviewAction()
+    // 1 address
+    public function addressAction()
     {
-        return $this->render('MsiStoreBundle:Checkout:review.html.twig', [
-            'calculator' => $this->get('msi_store.calculator'),
+        // faire un form pis mettre les infos du user sil est logger ou lui suggerer de logger
+        $builder = $this->get('form.factory')->createBuilder();
+
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('phone')
+            ->add('ext')
+            ->add('shippingCity')
+            ->add('shippingAddress')
+            ->add('shippingAddress2')
+            ->add('shippingProvince')
+            ->add('shippingCountry')
+            ->add('shippingZip')
+            ->add('billingCity')
+            ->add('billingAddress')
+            ->add('billingAddress2')
+            ->add('billingProvince')
+            ->add('billingCountry')
+            ->add('billingZip')
+        ;
+
+        $form = $builder->getForm();
+
+        if ($this->getUser()) {
+            $form->setData([
+                'firstName' => $this->getUser()->getFirstName(),
+                'lastName' => $this->getUser()->getLastName(),
+                'phone' => $this->getUser()->getPhone(),
+                'ext' => $this->getUser()->getExt(),
+                'shippingCity' => $this->getUser()->getShippingCity(),
+                'shippingAddress' => $this->getUser()->getShippingAddress(),
+                'shippingAddress2' => $this->getUser()->getShippingAddress2(),
+                'shippingProvince' => $this->getUser()->getShippingProvince(),
+                'shippingCountry' => $this->getUser()->getShippingCountry(),
+                'shippingZip' => $this->getUser()->getShippingZip(),
+                'billingCity' => $this->getUser()->getBillingCity(),
+                'billingAddress' => $this->getUser()->getBillingAddress(),
+                'billingAddress2' => $this->getUser()->getBillingAddress2(),
+                'billingProvince' => $this->getUser()->getBillingProvince(),
+                'billingCountry' => $this->getUser()->getBillingCountry(),
+                'billingZip' => $this->getUser()->getBillingZip(),
+            ]);
+        }
+
+        return $this->render('MsiStoreBundle:Checkout:address.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 
+    // 2 payment
     public function paymentAction()
+    {
+        return $this->render('MsiStoreBundle:Checkout:payment.html.twig');
+    }
+
+    // 3 process
+    public function processAction()
+    {
+        return $this->render('MsiStoreBundle:Checkout:payment.html.twig');
+    }
+
+    // 4 confirmation
+    public function confirmationAction()
     {
         return $this->render('MsiStoreBundle:Checkout:payment.html.twig');
     }
